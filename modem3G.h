7468d32681ffd98d07034d88213ee481b51328b7
Igor Kramer Pinotti <ipinotti@ipinotti.(none)>
2010-04-12 17:02:08 -0300

    ADDED modem3g.c  -   modem3g.h

---
+++ modem3G.h
@@ -0,0 +1,13 @@
+/*
+ * modem3G.h
+ *
+ *  Created on: Apr 12, 2010
+ *      Author: ipinotti
+ */
+
+int get_APN (char * apn);
+int set_APN (char * apn);
+int get_USERNAME (char * username);
+int set_USERNAME (char * username);
+int get_PASSWORD (char * password);
+int set_PASSWORD (char * password);
