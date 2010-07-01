458007fcc9b6a785fef36e75bc257f246989ca85
Thomás Alimena Del Grande <tgrande@pd3.com.br>
2010-07-01 17:44:31 -0300

    - shutdown functions.

--- src/dev.h
+++ src/dev.h
@@ -29,6 +29,9 @@ int libconfig_arp_del(char *host);
 int libconfig_arp_add(char *host, char *mac);
 int notify_driver_about_shutdown(char *dev);
 
+int libconfig_dev_shutdown(char *dev);
+int libconfig_dev_noshutdown(char *dev);
+
 #ifdef CONFIG_BUFFERS_USE_STATS
 int libconfig_dev_interface_get_buffers_use(char *dev,
                                             char *tx,
