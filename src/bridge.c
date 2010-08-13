f95972e95b81012e173e98c5056466ab0de5bbca
Thomás Alimena Del Grande <tgrande@pd3.com.br>
2010-08-13 15:29:17 -0300

    - Added libbridge to librouter. - Added bridge.c and bridge.h that uses libbridge API.

---
+++ src/bridge.c
@@ -0,0 +1,499 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <syslog.h>
+#include <asm/param.h>
+
+#include "error.h"
+#include "device.h"
+#include "libbridge/libbridge.h"
+
+int librouter_br_initbr(void)
+{
+	int err;
+
+	err = br_init();
+	if (err)
+		return (-err);
+	else
+		return 0;
+}
+
+int librouter_br_exists(char *brname)
+{
+	struct bridge *br;
+
+	if (br_refresh())
+		return 0;
+
+	br = br_find_bridge(brname);
+
+	return (br != NULL);
+}
+
+static struct bridge *_find_bridge(char *brname)
+{
+	struct bridge *br;
+
+	if (br_refresh())
+		return NULL;
+
+	br = br_find_bridge(brname);
+	if (br == NULL)
+		librouter_pr_error(0, "bridge %s doesn't exist", brname);
+	return br;
+}
+
+int librouter_br_addbr(char *brname)
+{
+	int err;
+
+	if ((err = br_add_bridge(brname)) == 0)
+		return 0;
+
+	switch (err) {
+	case EEXIST:
+		librouter_pr_error(0, "device %s already exists; can't create "
+			"bridge with the same name", brname);
+		break;
+
+	default:
+		librouter_pr_error(1, "br_add_bridge");
+		break;
+	}
+	return (-err);
+}
+
+int librouter_br_delbr(char *brname)
+{
+	int err;
+
+	if ((err = br_del_bridge(brname)) == 0)
+		return 0;
+
+	switch (err) {
+	case ENXIO:
+		librouter_pr_error(0, "bridge %s doesn't exist; can't delete it", brname);
+		break;
+
+	case EBUSY:
+		librouter_pr_error(0, "bridge %s is still up; can't delete it", brname);
+		break;
+
+	default:
+		librouter_pr_error(1, "br_del_bridge");
+		break;
+	}
+	return (-err);
+}
+
+int librouter_br_addif(char *brname, char *ifname)
+{
+	int err;
+	int ifindex;
+	struct bridge *br = _find_bridge(brname);
+	if (br == NULL)
+		return (-1);
+
+	ifindex = if_nametoindex(ifname);
+	if (!ifindex) {
+		librouter_pr_error(0, "interface %s does not exist!", ifname);
+		return (-1);
+	}
+
+	if ((err = br_add_interface(br, ifindex)) == 0)
+		return 0;
+
+	switch (err) {
+	case EBUSY:
+		librouter_pr_error(0, "device %s is already a member of a bridge; "
+			"can't enslave it to bridge %s", ifname, br->ifname);
+		break;
+
+	default:
+		librouter_pr_error(1, "br_add_interface");
+		break;
+	}
+	return (-err);
+}
+
+int librouter_br_delif(char *brname, char *ifname)
+{
+	int err;
+	int ifindex;
+	struct bridge *br = _find_bridge(brname);
+	if (br == NULL)
+		return (-1);
+
+	ifindex = if_nametoindex(ifname);
+	if (!ifindex) {
+		librouter_pr_error(0, "interface %s does not exist!", ifname);
+		return (-1);
+	}
+
+	if ((err = br_del_interface(br, ifindex)) == 0)
+		return 0;
+
+	switch (err) {
+	case EINVAL:
+		librouter_pr_error(0, "device %s is not a slave of %s", ifname, br->ifname);
+		break;
+
+	default:
+		librouter_pr_error(1, "br_del_interface");
+		break;
+	}
+	return (-err);
+}
+
+int librouter_br_checkif(char *brname, char *ifname)
+{
+	struct port *p;
+	struct bridge *br;
+	char buf[IFNAMSIZ];
+
+	if (br_refresh())
+		return (0);
+
+	br = br_find_bridge(brname);
+	if (br == NULL)
+		return (0);
+
+	p = br->firstport;
+
+	while (p != NULL) {
+		if (strcmp(ifname, if_indextoname(p->ifindex, buf)) == 0)
+			return 1;
+		p = p->next;
+	}
+	return 0;
+}
+
+int librouter_br_hasifs(char *brname)
+{
+	struct bridge *br = _find_bridge(brname);
+	if (br == NULL)
+		return 0;
+	if (br->firstport == NULL)
+		return 0;
+	return 1;
+}
+
+int librouter_br_setageing(char *brname, int sec)
+{
+	int err;
+	struct timeval tv;
+	struct bridge *br = _find_bridge(brname);
+	if (br == NULL)
+		return (-1);
+
+	tv.tv_sec = sec;
+	tv.tv_usec = 0;
+	err = br_set_ageing_time(br, &tv);
+	if (err)
+		return (-err);
+	else
+		return 0;
+}
+
+int librouter_br_getageing(char *brname)
+{
+	struct bridge *br = _find_bridge(brname);
+	if (br == NULL)
+		return (-1);
+	return br->info.ageing_time.tv_sec;
+}
+
+int librouter_br_setbridgeprio(char *brname, int prio)
+{
+	int err;
+	struct bridge *br = _find_bridge(brname);
+	if (br == NULL)
+		return (-1);
+
+	err = br_set_bridge_priority(br, prio);
+	if (err)
+		return (-err);
+	else
+		return 0;
+}
+
+int librouter_br_getbridgeprio(char *brname)
+{
+	struct bridge *br = _find_bridge(brname);
+	if (br == NULL)
+		return (-1);
+	return ((br->info.bridge_id.prio[0] << 8) + br->info.bridge_id.prio[1]);
+}
+
+int librouter_br_setfd(char *brname, int sec)
+{
+	int err;
+	struct timeval tv;
+	struct bridge *br = _find_bridge(brname);
+	if (br == NULL)
+		return (-1);
+
+	tv.tv_sec = sec;
+	tv.tv_usec = 0;
+
+	err = br_set_bridge_forward_delay(br, &tv);
+	if (err)
+		return (-err);
+	else
+		return 0;
+}
+
+int librouter_br_getfd(char *brname)
+{
+	struct bridge *br = _find_bridge(brname);
+	if (br == NULL)
+		return (-1);
+	return br->info.bridge_forward_delay.tv_sec;
+}
+
+int librouter_br_setgcint(char *brname, int sec)
+{
+	int err;
+	struct timeval tv;
+	struct bridge *br = _find_bridge(brname);
+	if (br == NULL)
+		return (-1);
+
+	tv.tv_sec = sec;
+	tv.tv_usec = 0;
+
+	err = br_set_gc_interval(br, &tv);
+	if (err)
+		return (-err);
+	else
+		return 0;
+}
+
+int librouter_br_sethello(char *brname, int sec)
+{
+	int err;
+	struct timeval tv;
+	struct bridge *br = _find_bridge(brname);
+	if (br == NULL)
+		return (-1);
+
+	tv.tv_sec = sec;
+	tv.tv_usec = 0;
+
+	err = br_set_bridge_hello_time(br, &tv);
+	if (err)
+		return (-err);
+	else
+		return 0;
+}
+
+int librouter_librouter_br_gethello(char *brname)
+{
+	struct bridge *br = _find_bridge(brname);
+	if (br == NULL)
+		return (-1);
+	return br->info.bridge_hello_time.tv_sec;
+}
+
+int librouter_br_setmaxage(char *brname, int sec)
+{
+	int err;
+	struct timeval tv;
+	struct bridge *br = _find_bridge(brname);
+	if (br == NULL)
+		return (-1);
+
+	tv.tv_sec = sec;
+	tv.tv_usec = 0;
+
+	err = br_set_bridge_max_age(br, &tv);
+	if (err)
+		return (-err);
+	else
+		return 0;
+}
+
+int librouter_br_getmaxage(char *brname)
+{
+	struct bridge *br = _find_bridge(brname);
+	if (br == NULL)
+		return (-1);
+	return br->info.bridge_max_age.tv_sec;
+}
+
+int librouter_br_setpathcost(char *brname, char *portname, int cost)
+{
+	struct port *p;
+	int err;
+	struct bridge *br = _find_bridge(brname);
+	if (br == NULL)
+		return (-1);
+
+	if ((p = br_find_port(br, portname)) == NULL) {
+		librouter_pr_error(0, "can't find port %s in bridge %s", portname, br->ifname);
+		return (-1);
+	}
+
+	err = br_set_path_cost(p, cost);
+	if (err)
+		return (-err);
+	else
+		return 0;
+}
+
+int librouter_br_setportprio(char *brname, char *portname, int cost)
+{
+	struct port *p;
+	int err;
+	struct bridge *br = _find_bridge(brname);
+	if (br == NULL)
+		return (-1);
+
+	if ((p = br_find_port(br, portname)) == NULL) {
+		librouter_pr_error(0, "can't find port %s in bridge %s", portname, br->ifname);
+		return (-1);
+	}
+
+	err = br_set_port_priority(p, cost);
+	if (err)
+		return (-err);
+	else
+		return 0;
+}
+
+int librouter_br_set_stp(char *brname, int on_off)
+{
+	int err;
+	struct bridge *br = _find_bridge(brname);
+	if (br == NULL)
+		return (-1);
+
+	err = br_set_stp_state(br, on_off);
+	if (err)
+		return (-err);
+	else
+		return 0;
+}
+
+int librouter_br_get_stp(char *brname)
+{
+	struct bridge *br = _find_bridge(brname);
+	if (br == NULL)
+		return (-1);
+	return br->info.stp_enabled;
+}
+
+static void librouter_br_dump_bridge_id(unsigned char *x, FILE *out)
+{
+	fprintf(out, "%.2x%.2x.%.2x%.2x%.2x%.2x%.2x%.2x", x[0], x[1], x[2], x[3], x[4], x[5], x[6],
+	                x[7]);
+}
+
+static void librouter_br_show_timer(struct timeval *tv, FILE *out)
+{
+	fprintf(out, "%4i", (int) tv->tv_sec);
+}
+
+static void librouter_br_dump_port_info(struct port *p, FILE *out)
+{
+	char ifname[IFNAMSIZ];
+	struct port_info *pi;
+	char *dev;
+
+	pi = &p->info;
+	if_indextoname(p->ifindex, ifname);
+	dev = librouter_device_convert_os(ifname, 1);
+	if (dev == NULL)
+		return;
+
+	fprintf(out, "%s (%i)\n", dev, p->index);
+	fprintf(out, " port id\t\t%.4x\t\t\t", pi->port_id);
+	fprintf(out, "state\t\t\t%s\n", br_get_state_name(pi->state));
+	fprintf(out, " designated root\t");
+	librouter_br_dump_bridge_id((unsigned char *) &pi->designated_root, out);
+	fprintf(out, "\tpath cost\t\t%4i\n", pi->path_cost);
+
+	fprintf(out, " designated bridge\t");
+	librouter_br_dump_bridge_id((unsigned char *) &pi->designated_bridge, out);
+	fprintf(out, "\tmessage age timer\t");
+	librouter_br_show_timer(&pi->message_age_timer_value, out);
+	fprintf(out, "\n designated port\t%.4x", pi->designated_port);
+	fprintf(out, "\t\t\tforward delay timer\t");
+	librouter_br_show_timer(&pi->forward_delay_timer_value, out);
+	fprintf(out, "\n designated cost\t%4i", pi->designated_cost);
+	fprintf(out, "\t\t\thold timer\t\t");
+	librouter_br_show_timer(&pi->hold_timer_value, out);
+	fprintf(out, "\n flags\t\t\t");
+	if (pi->config_pending)
+		fprintf(out, "CONFIG_PENDING ");
+	if (pi->top_change_ack)
+		fprintf(out, "TOPOLOGY_CHANGE_ACK ");
+	fprintf(out, "\n");
+	fprintf(out, "\n");
+}
+
+int librouter_br_dump_info(char *brname, FILE *out)
+{
+	struct bridge *br = _find_bridge(brname);
+	struct bridge_info *bri;
+	struct port *p;
+
+	if (br == NULL)
+		return (-1);
+	bri = &br->info;
+
+	fprintf(out, "%s\n", br->ifname);
+	if (!bri->stp_enabled) {
+		fprintf(out, " STP is disabled for this interface\n");
+		return 0;
+	}
+
+	fprintf(out, " bridge id\t\t");
+	librouter_br_dump_bridge_id((unsigned char *) &bri->bridge_id, out);
+	fprintf(out, "\n designated root\t");
+	librouter_br_dump_bridge_id((unsigned char *) &bri->designated_root, out);
+	fprintf(out, "\n root port\t\t%4i\t\t\t", bri->root_port);
+	fprintf(out, "path cost\t\t%4i\n", bri->root_path_cost);
+	fprintf(out, " max age\t\t");
+	librouter_br_show_timer(&bri->max_age, out);
+	fprintf(out, "\t\t\tbridge max age\t\t");
+	librouter_br_show_timer(&bri->bridge_max_age, out);
+	fprintf(out, "\n hello time\t\t");
+	librouter_br_show_timer(&bri->hello_time, out);
+	fprintf(out, "\t\t\tbridge hello time\t");
+	librouter_br_show_timer(&bri->bridge_hello_time, out);
+	fprintf(out, "\n forward delay\t\t");
+	librouter_br_show_timer(&bri->forward_delay, out);
+	fprintf(out, "\t\t\tbridge forward delay\t");
+	librouter_br_show_timer(&bri->bridge_forward_delay, out);
+	fprintf(out, "\n ageing time\t\t");
+	librouter_br_show_timer(&bri->ageing_time, out);
+	fprintf(out, "\t\t\tgc interval\t\t");
+	librouter_br_show_timer(&bri->gc_interval, out);
+	fprintf(out, "\n hello timer\t\t");
+	librouter_br_show_timer(&bri->hello_timer_value, out);
+	fprintf(out, "\t\t\ttcn timer\t\t");
+	librouter_br_show_timer(&bri->tcn_timer_value, out);
+	fprintf(out, "\n topology change timer\t");
+	librouter_br_show_timer(&bri->topology_change_timer_value, out);
+	fprintf(out, "\t\t\tgc timer\t\t");
+	librouter_br_show_timer(&bri->gc_timer_value, out);
+	fprintf(out, "\n flags\t\t\t");
+	if (bri->topology_change)
+		fprintf(out, "TOPOLOGY_CHANGE ");
+	if (bri->topology_change_detected)
+		fprintf(out, "TOPOLOGY_CHANGE_DETECTED ");
+	fprintf(out, "\n");
+	fprintf(out, "\n");
+	fprintf(out, "\n");
+
+	p = br->firstport;
+	while (p != NULL) {
+		librouter_br_dump_port_info(p, out);
+		p = p->next;
+	}
+	return 0;
+}
+
---
+++ src/libbridge/libbridge_init.c
@@ -0,0 +1,243 @@
+/*
+ * Copyright (C) 2000 Lennert Buytenhek
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/time.h>
+#include "libbridge.h"
+#include "libbridge_private.h"
+
+#define MAX_BRIDGES	1024	/* arbitrary */
+#define MAX_PORTS	256	/* STP limitation */
+
+int br_socket_fd;
+struct bridge *bridge_list;
+
+static void __bridge_info_copy(struct bridge_info *info, 
+			       const struct __bridge_info *i)
+{
+	memcpy(&info->designated_root, &i->designated_root, 8);
+	memcpy(&info->bridge_id, &i->bridge_id, 8);
+	info->root_path_cost = i->root_path_cost;
+	info->topology_change = i->topology_change;
+	info->topology_change_detected = i->topology_change_detected;
+	info->root_port = i->root_port;
+	info->stp_enabled = i->stp_enabled;
+	__jiffies_to_tv(&info->max_age, i->max_age);
+	__jiffies_to_tv(&info->hello_time, i->hello_time);
+	__jiffies_to_tv(&info->forward_delay, i->forward_delay);
+	__jiffies_to_tv(&info->bridge_max_age, i->bridge_max_age);
+	__jiffies_to_tv(&info->bridge_hello_time, i->bridge_hello_time);
+	__jiffies_to_tv(&info->bridge_forward_delay, i->bridge_forward_delay);
+	__jiffies_to_tv(&info->ageing_time, i->ageing_time);
+	__jiffies_to_tv(&info->gc_interval, i->gc_interval);
+	__jiffies_to_tv(&info->hello_timer_value, i->hello_timer_value);
+	__jiffies_to_tv(&info->tcn_timer_value, i->tcn_timer_value);
+	__jiffies_to_tv(&info->topology_change_timer_value,
+			i->topology_change_timer_value);
+	__jiffies_to_tv(&info->gc_timer_value, i->gc_timer_value);
+}
+
+static void __port_info_copy(struct port_info *info, 
+			     const struct __port_info *i)
+{
+	memcpy(&info->designated_root, &i->designated_root, 8);
+	memcpy(&info->designated_bridge, &i->designated_bridge, 8);
+	info->port_id = i->port_id;
+	info->designated_port = i->designated_port;
+	info->path_cost = i->path_cost;
+	info->designated_cost = i->designated_cost;
+	info->state = i->state;
+	info->top_change_ack = i->top_change_ack;
+	info->config_pending = i->config_pending;
+	__jiffies_to_tv(&info->message_age_timer_value,
+			i->message_age_timer_value);
+	__jiffies_to_tv(&info->forward_delay_timer_value,
+			i->forward_delay_timer_value);
+	__jiffies_to_tv(&info->hold_timer_value,
+			i->hold_timer_value);
+}
+
+static int br_read_info(struct bridge *br)
+{
+	struct __bridge_info i;
+
+	if (if_indextoname(br->ifindex, br->ifname) == NULL) {
+		/* index was there, but now it is gone! */
+		return -1;
+	}
+
+	if (br_device_ioctl(br, BRCTL_GET_BRIDGE_INFO,
+			    (unsigned long)&i, 0, 0) < 0) {
+		fprintf(stderr, "%s: can't get info %s\n",
+			br->ifname, strerror(errno));
+		return errno;
+	}
+
+	__bridge_info_copy(&br->info, &i);
+	return 0;
+}
+
+static int br_read_port_info(struct port *p)
+{
+	struct __port_info i;
+
+	if (br_device_ioctl(p->parent, BRCTL_GET_PORT_INFO,
+			    (unsigned long)&i, p->index, 0) < 0) {
+		fprintf(stderr, "%s: can't get port %d info %s\n",
+			p->parent->ifname, p->index, strerror(errno));
+		return errno;
+	}
+
+	__port_info_copy(&p->info, &i);
+	return 0;
+}
+
+static void br_nuke_bridge(struct bridge *b)
+{
+	struct port *p, *n;
+
+	for (p = b->firstport; p; p = n) {
+		n = p->next;
+		free(p);
+	}
+
+	free(b);
+}
+
+static int br_make_port_list(struct bridge *br)
+{
+	int err;
+	int i;
+	int ifindices[MAX_PORTS];
+	struct port *p, **top;
+
+	memset(ifindices, 0, sizeof(ifindices));
+	if (br_device_ioctl(br, BRCTL_GET_PORT_LIST, (unsigned long)ifindices,
+			    MAX_PORTS, 0) < 0)
+		return errno;
+
+	top = &br->firstport;
+	for (i = 0; i < MAX_PORTS; i++) {
+		if (!ifindices[i])
+			continue;
+
+		p = malloc(sizeof(struct port));
+		if (!p) {
+			err = -ENOMEM;
+			goto error_out;
+		}
+
+		p->next = NULL;
+		p->ifindex = ifindices[i];
+		p->parent = br;
+		p->index = i;
+		*top = p;
+		top = &p->next;
+
+		if ((err = br_read_port_info(p)) != 0)
+			goto error_out;
+	}
+
+	return 0;
+
+ error_out:
+	p = br->firstport;
+	while (p) {
+		struct port *n = p->next;
+		free(p);
+		p = n;
+	}
+	br->firstport = NULL;
+
+	return err;
+}
+
+static int br_make_bridge_list(void)
+{
+	int i, num;
+	int ifindices[MAX_BRIDGES];
+
+	num = br_get_br(BRCTL_GET_BRIDGES, (unsigned long)ifindices, 
+			MAX_BRIDGES);
+	if (num < 0) {
+		fprintf(stderr, "Get bridge indices failed: %s\n",
+			strerror(errno));
+		return errno;
+	}
+	bridge_list = NULL;
+	for (i = 0;i < num; i++) {
+		struct bridge *br;
+
+		br = malloc(sizeof(struct bridge));
+		if (!br)
+			return -ENOMEM;
+
+		memset(br, 0, sizeof(struct bridge));
+		br->ifindex = ifindices[i];
+		br->firstport = NULL;
+		if ( br_read_info(br) || br_make_port_list(br)) {
+			/* ignore the problem could just be a race! */
+			free(br);
+			continue;
+		}
+
+		br->next = bridge_list;
+		bridge_list = br;
+	}
+
+	return 0;
+}
+
+int br_init()
+{
+	int err;
+
+	if ((br_socket_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+		return errno;
+
+	if (br_get_version() != BRCTL_VERSION) {
+		fprintf(stderr, "bridge utilities not compatiable with kernel version\n");
+		exit(1);
+	}
+
+	if ((err = br_make_bridge_list()) != 0)
+		return err;
+
+	return 0;
+}
+
+int br_refresh()
+{
+	struct bridge *b;
+
+	b = bridge_list;
+	while (b != NULL) {
+		struct bridge *bnext;
+
+		bnext = b->next;
+		br_nuke_bridge(b);
+		b = bnext;
+	}
+
+	return br_make_bridge_list();
+}
