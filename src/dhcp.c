f9abda4653ac752f756d773a38c666ad03803d55
Thomás Alimena Del Grande <tgrande@pd3.com.br>
2011-07-19 19:06:12 -0300

    Make DHCP Server and Bridge to be mutually exclusive on interface Ethernet 0

--- src/dhcp.c
+++ src/dhcp.c
@@ -30,6 +30,7 @@
 #include "exec.h"
 #include "defines.h"
 #include "dev.h"
+#include "device.h"
 #include "ip.h"
 #include "ipsec.h"
 #include "process.h"
@@ -518,6 +519,8 @@ int librouter_dhcp_server_set(int enable)
 	if (enable) {
 		char filename[64];
 		FILE *file;
+		dev_family *fam = librouter_device_get_family_by_type(eth);
+		char iface[64];
 
 		sprintf(filename, FILE_DHCPDLEASES, INTF_DHCP_SERVER_DEFAULT);
 		file = fopen(filename, "r");
@@ -531,6 +534,13 @@ int librouter_dhcp_server_set(int enable)
 		/* se o dhcrelay estiver rodando, tira do ar! */
 		if (librouter_dhcp_get_status() == DHCP_RELAY)
 			librouter_dhcp_set_no_relay();
+
+		sprintf(iface, "%s%d", fam->linux_string, INTF_DHCP_SERVER_DEFAULT);
+		if (librouter_br_is_interface_enslaved(iface)) {
+			sprintf(iface, "%s%d", fam->cish_string, INTF_DHCP_SERVER_DEFAULT);
+			printf("%% Bridge is active on interface %s\n", iface);
+			return -1;
+		}
 
 		/* poe o dhcpd para rodar */
 		return librouter_dhcpd_set_status(1, INTF_DHCP_SERVER_DEFAULT);
