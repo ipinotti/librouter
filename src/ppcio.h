0f1845724c590332ee96258bbacf5105cb9793f7
Thomás Alimena Del Grande <tgrande@pd3.com.br>
2011-11-18 17:24:03 -0200

    Fix ppcio include for MRG board

--- src/ppcio.h
+++ src/ppcio.h
@@ -7,7 +7,17 @@
 #ifndef PPCIO_H_
 #define PPCIO_H_
 
+/*
+ * For the MRG, we use a kernel module (outside the
+ * kernel source tree), and the header file is simply
+ * installed at the include directory. Remove the linux
+ * include when all boards have ported to the same kernel module.
+ */
+#ifdef CONFIG_DIGISTAR_MRG
+#include <ppc_io.h>
+#else
 #include <linux/asm/ppc_io.h>
+#endif
 
 int librouter_ppcio_read(struct powerpc_gpio *gpio);
 int librouter_ppcio_write(struct powerpc_gpio *gpio);
